51b7e45b875f5f439c89894f1e73f2d2dff48f7a
geodebreaker <geodebreaker@users.noreply.github.com>
2025-10-08 21:05:31 -0500

    diddy

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 if (location.pathname == '/') location.pathname = '/index.html';
+
+function uk() {
+  let e = document.createElement('iframe');
+  e.src = "/ass/ultrakill.html";
+  document.querySelector('#ultrapilled').appendChild(e);
+  document.querySelector('#ultrabutton').remove();
+}
