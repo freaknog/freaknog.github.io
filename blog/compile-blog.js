51a51655909251b8b56f53c89b13a77a186aa9a9
geodebreaker <geodebreaker@users.noreply.github.com>
2025-10-12 12:36:30 -0500

    +rizz

---
+++ blog/compile-blog.js
@@ -0,0 +1,25 @@
+const url = process.argv[2];
+const fs = require('fs');
+const blog = JSON.parse(fs.readFileSync('blog.json').toString());
+
+let items = blog.sort((a, b) => Number(b.time) - Number(a.time)).map(item => `<item>
+      <guid>${item.id}</guid>
+      <title><![CDATA[${item.title}]]></title>
+      <description></description>
+      <pubDate>${new Date(Number(item.time)).toUTCString()}</pubDate>
+      <link>${url}blog.html#${encodeURIComponent(item.id)}</link>
+    </item>`).join("\n");
+
+fs.writeFileSync('rss.xml', `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+  <channel>
+    <title>freaknog supreme</title>
+    <link>${url}blog.html</link>
+    <description>oh yeah baby you know im freaknogging it</description>
+    <language>en-us</language>
+    <lastBuildDate>${new Date(
+  Math.max(...posts.map(item => Number(item.time)))
+).toUTCString()}</lastBuildDate>
+    ${items}
+  </channel>
+</rss>`);
--- script.js
+++
@@ -1,8 +0,0 @@
-if (location.pathname == '/') location.pathname = '/index.html';
-
-function embed(x) {
-  let e = document.createElement('iframe');
-  e.src = "/embed/" + new URL(x.href).hash.replace('#', '') + ".html";
-  x.parentElement.insertBefore(e, x);
-  x.remove();
-}
