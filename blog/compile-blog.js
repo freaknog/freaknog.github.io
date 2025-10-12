939521905ec80f8460b47caed05a92ce62bf6c17
geodebreaker <geodebreaker@users.noreply.github.com>
2025-10-12 12:39:29 -0500

    who up pissing they shit rn

--- blog/compile-blog.js
+++ blog/compile-blog.js
@@ -2,14 +2,6 @@ const url = process.argv[2];
 const fs = require('fs');
 const blog = JSON.parse(fs.readFileSync('blog.json').toString());
 
-let items = blog.sort((a, b) => Number(b.time) - Number(a.time)).map(item => `<item>
-      <guid>${item.id}</guid>
-      <title><![CDATA[${item.title}]]></title>
-      <description></description>
-      <pubDate>${new Date(Number(item.time)).toUTCString()}</pubDate>
-      <link>${url}blog.html#${encodeURIComponent(item.id)}</link>
-    </item>`).join("\n");
-
 fs.writeFileSync('rss.xml', `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
   <channel>
@@ -17,9 +9,13 @@ fs.writeFileSync('rss.xml', `<?xml version="1.0" encoding="UTF-8" ?>
     <link>${url}blog.html</link>
     <description>oh yeah baby you know im freaknogging it</description>
     <language>en-us</language>
-    <lastBuildDate>${new Date(
-  Math.max(...items.map(item => Number(item.time)))
-).toUTCString()}</lastBuildDate>
-    ${items}
+    <lastBuildDate>${new Date(Math.max(...blog.map(item => Number(item.time)))).toUTCString()}</lastBuildDate>
+    ${blog.sort((a, b) => Number(b.time) - Number(a.time)).map(item => `<item>
+      <guid>${item.id}</guid>
+      <title><![CDATA[${item.title}]]></title>
+      <description></description>
+      <pubDate>${new Date(Number(item.time)).toUTCString()}</pubDate>
+      <link>${url}blog.html#${encodeURIComponent(item.id)}</link>
+    </item>`).join("\n")}
   </channel>
 </rss>`);
